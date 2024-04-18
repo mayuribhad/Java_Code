ce0d202e3a14a6edd47f02f72a13f7f4dc08c506
Mayuri Bhad <91111021+mayuribhad@users.noreply.github.com>
2024-04-18 09:03:44 +0530

    Add files via upload

---
+++ lec25_string_count_vowels.java
@@ -0,0 +1,20 @@
+public class lec25_string_count_vowels {
+    public static void main(String[] args) {
+
+        /*string "Rani"
+         * output - 2
+         */
+
+         
+        String s1 = "Rani";
+        int count = 0;
+        for(int i = 0;i<s1.length();i++){
+            char c = s1.charAt(i);
+            if(c == 'a'|| c=='e'||c == 'i'|| c == 'o'|| c == 'u'|| c == 'A'|| c == 'E'|| c == 'I'|| c == 'O'|| c == 'U')
+            count++;
+
+        }
+        System.out.println(count);
+    }
+    
+}
